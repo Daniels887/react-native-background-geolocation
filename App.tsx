c6af21e0794946d2bbab1367ef788080be3f2869
Daniel <daniel_sousa.p@outlook.com>
2020-11-28 19:31:13 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync()
+  
+      if(status !== 'granted') {
+        console.log('Permission to acces location was denied');
+      }
+      
+    })()
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
