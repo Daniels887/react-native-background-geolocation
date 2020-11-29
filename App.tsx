139a375b82fe60635b86be2fc277d29f4674cb2f
Daniel <daniel_sousa.p@outlook.com>
2020-11-29 18:57:01 -0300

    make polyline

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text } from 'react-native';
 import * as Location from 'expo-location';
 import * as TaskManager from 'expo-task-manager';
+import MapView, { Marker, Polyline } from 'react-native-maps'
 
 export default function App() {
   const [location, setLocations] = useState([{
-    coords: {
-      accuracy: 0,
-      altitude: 0,
-      altitudeAccuracy: 0,
-      heading: 0,
       latitude: 0,
       longitude: 0,
-      speed: 0,
-    },
   }])
+
+  useEffect(() => {
+    console.log(location)
+  }, [location])
 
   useEffect(() => {
     (async () => {
@@ -24,11 +21,11 @@ export default function App() {
       if(status === 'granted') {
         await Location.startLocationUpdatesAsync('first', {
           accuracy: Location.Accuracy.Balanced,
-          timeInterval: 5000,
+          timeInterval: 10000,
           distanceInterval: 0,
           foregroundService: {
             notificationTitle: 'First',
-            notificationBody: `${location[0].coords.longitude}` ,
+            notificationBody: 'Você está sendo monitorado' ,
           },
           pausesUpdatesAutomatically: false
         })
@@ -43,24 +40,39 @@ export default function App() {
     }
   
     if(data ) {
-      setLocations(data.locations)
+      const currentLocation = {
+        latitude: data.locations[0].coords.latitude,
+        longitude: data.locations[0].coords.longitude
+      }
+      setLocations([...location, currentLocation ])
     }
   })
 
   return (
-    <View style={styles.container}>
-      <Text>{location[0].coords.longitude}</Text>
-      <StatusBar style="auto" />
-    </View>
+      <MapView 
+      style={styles.map}
+      initialRegion={{
+      latitude: -14.2374362,
+      longitude: -60.3452921,
+      latitudeDelta: 0.04,
+      longitudeDelta: 0.04,
+    }}>
+        <Polyline coordinates={[
+			{ latitude: -23.4390556, longitude: -46.5184384 },
+		]} />
+    </MapView>
   );
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  map: {
+    flex: 1
   },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 27
+  },
+
 });
